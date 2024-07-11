11a3d1215bbff7350da253a93a6b1b621fac4c81
Baldomero <baldomero.napoli@gmail.com>
2024-07-11 13:20:12 -0400

    se modifica la forma en la que se injectan los viewmodel y casos de uso

--- shared/src/commonMain/kotlin/com/baldomeronapoli/eventplanner/di/UseCaseModule.kt
+++ shared/src/commonMain/kotlin/com/baldomeronapoli/eventplanner/di/UseCaseModule.kt
@@ -1,8 +1,9 @@
 package com.baldomeronapoli.eventplanner.di
 
 import com.baldomeronapoli.eventplanner.domain.usecases.GetGreetingUseCase
+import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
 
 val useCaseModule = module {
-    single<GetGreetingUseCase> { GetGreetingUseCase(get()) }
+    factoryOf(::GetGreetingUseCase)
 }
--- shared/src/commonMain/kotlin/com/baldomeronapoli/eventplanner/di/ViewModelModule.kt
+++ shared/src/commonMain/kotlin/com/baldomeronapoli/eventplanner/di/ViewModelModule.kt
@@ -1,8 +1,9 @@
 package com.baldomeronapoli.eventplanner.di
 
 import com.baldomeronapoli.eventplanner.presentation.GreetingViewModel
+import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
 
 val viewModelModule = module {
-    single<GreetingViewModel> { GreetingViewModel(get()) }
+    factoryOf(::GreetingViewModel)
 }
