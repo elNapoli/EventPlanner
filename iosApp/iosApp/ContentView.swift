559147ec57b6269fac02cc2999bff9f5c98c745c
Baldomero <baldomero.napoli@gmail.com>
2024-07-15 20:07:52 -0400

    se corrigen todos los errores relacionados al MVI en IOS, en este momento el MVI esta al 90% solo falta escuchar los sideEffect del viewModel

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -6,15 +6,33 @@ extension ContentView {
     @MainActor
     class GreetingViewModelWrapper: ObservableObject {
        
-
+        @Published var uiState =  GreetingContractUiState.companion.initialUiState()
         private let viewModel: GreetingViewModel
-  
+        private var stateSubscription: KmmSubscription!
         
 
         init() {
             viewModel = ViewModelInjector().greetingViewModel
+            subscribeState()
         }
 
+        func sendEvent(event: GreetingContractUiAction) {
+                viewModel.onAction(uiAction: event)
+        }
+
+        private func subscribeState() {
+            stateSubscription = viewModel.uiState.subscribe(
+                onEach: { state in
+                    self.uiState = state!
+                },
+                onCompletion: { error in
+                    if let error = error {
+                        print(error)
+                    }
+                }
+            )
+        }
+        
 
     }
 }
@@ -23,17 +41,21 @@ struct ContentView: View {
     @ObservedObject private(set) var viewModel: GreetingViewModelWrapper
 
     var body: some View {
-        VStack {
-            Text("adfadsf")
-        
-            Button(action: {
-              
-            }) {
-                Text("Buswcnado datos")
-            }
-        }
-    }
-}
+          VStack {
+              if viewModel.uiState.isLoading{
+                  Text("Cargando......")
+              }
+              else{
+                  Text(viewModel.uiState.data)
+              }
+          
+              Button(action: {
+                  viewModel.sendEvent(event: GreetingContractUiActionLoadGreeting())
+              }) {
+                  Text("Load Data")
+              }
+          }
+      }}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
